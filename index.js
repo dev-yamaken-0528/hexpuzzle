58827b5ce9c2aebac538572f6f5f8adf12050616
dev-yamaken-0528 <dev.yamaken.0528@gmail.com>
2018-09-09 21:01:24 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+  },
+  created: function(){
+  },
+  mounted: function(){
+  },
+  methods: {
+    clickhex: function(event){
+      console.log(event.target.id)
+    },
+  },
+})
+
+
