5cc39dfaca3c9ef6231d648da134ca5e527c6764
dev-yamaken-0528 <dev.yamaken.0528@gmail.com>
2018-09-09 21:50:52 +0900

    選択処理修正中

--- index.js
+++ index.js
@@ -8,7 +8,20 @@ var app = new Vue({
   },
   methods: {
     clickhex: function(event){
-      console.log(event.target.id)
+      var row = Number(event.target.id.split('_')[0])
+      var col = Number(event.target.id.split('_')[1])
+      var left = row + '_' + (col-1)
+      var right = row + '_' + (col+1)
+      var up_left = (row-1) + '_' + (col-1)
+      var up_right = (row-1) + '_' + (col)
+      var down_left = (row-1) + '_' + col
+      var down_right = row + '_' + col
+      console.log('----------')
+      console.log("target["+event.target.id+"]")
+      //document.getElementById(left).style.backgroundColor = "red"
+      //document.getElementById(right).style.backgroundColor = "red"
+      document.getElementById(up_left).style.backgroundColor = "red"
+      document.getElementById(up_right).style.backgroundColor = "red"
     },
   },
 })
