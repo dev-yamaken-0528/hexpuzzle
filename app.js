d6fa6615266f9ee6e8010435682c5c33432f59fe
dev-yamaken-0528 <dev.yamaken.0528@gmail.com>
2018-09-12 18:39:24 +0900

    隣接セルの処理追加

--- app.js
+++ app.js
@@ -45,10 +45,17 @@ var app = new Vue({
 
   },
   methods: {
+
     clickhex: function(event){
+      var sel_color = document.getElementById( event.target.id ).style.backgroundColor
+      this.set_rand_color( event.target.id )
+      this.adjacent_hex( event.target.id, sel_color )
+    },
+
+    adjacent_hex: function(target, sel_color){
       var delimiter = '_'
-      var row   = Number(event.target.id.split(delimiter)[0])
-      var col   = Number(event.target.id.split(delimiter)[1])
+      var row   = Number(target.split(delimiter)[0])
+      var col   = Number(target.split(delimiter)[1])
 
       var calc_max_num = 0
       if(this.level == 'easy'){
@@ -61,7 +68,7 @@ var app = new Vue({
         calc_max_num = 7
       }
 
-      var hex_id_c   = event.target.id
+      var hex_id_c   = target
       var hex_id_l   = row + delimiter + (col-1)
       var hex_id_r   = row + delimiter + (col+1)
       var hex_id_u_l = (row-1) + delimiter + (col-calc_max_num)
@@ -69,26 +76,29 @@ var app = new Vue({
       var hex_id_d_l = (row+1) + delimiter + (col+(calc_max_num-1))
       var hex_id_d_r = (row+1) + delimiter + (col+calc_max_num)
 
-      //if(document.getElementById(hex_id_l) != null){
-      //  document.getElementById(hex_id_l).style.backgroundColor = "red"
-      //}
-      //if(document.getElementById(hex_id_r) != null){
-      //  document.getElementById(hex_id_r).style.backgroundColor = "red"
-      //}
-      //if(document.getElementById(hex_id_u_l) != null){
-      //  document.getElementById(hex_id_u_l).style.backgroundColor = "blue"
-      //}
-      //if(document.getElementById(hex_id_u_r) != null){
-      //  document.getElementById(hex_id_u_r).style.backgroundColor = "blue"
-      //}
-      //if(document.getElementById(hex_id_d_l) != null){
-      //  document.getElementById(hex_id_d_l).style.backgroundColor = "green"
-      //}
-      //if(document.getElementById(hex_id_d_r) != null){
-      //  document.getElementById(hex_id_d_r).style.backgroundColor = "green"
-      //}
+      var hex_id_list = [ hex_id_c, hex_id_l, hex_id_r, hex_id_u_l, hex_id_u_r, hex_id_d_l, hex_id_d_r ]
+      for(var i=0; i<hex_id_list.length; i++){
+        var hex_id = hex_id_list[i]
+        if( document.getElementById( hex_id ) != null ){
+          if( document.getElementById( hex_id ).style.backgroundColor == sel_color ){
+            this.set_rand_color( hex_id )
+            this.adjacent_hex( hex_id, sel_color )
+          }
+        }
+      }
     },
+
+    set_rand_color: function( target ){
+      var min = 1
+      var max = 3
+      var rand = Math.floor( Math.random() * (max + 1 - min) ) + min
+      var bgcolor = ''
+      if(rand == 1){ bgcolor = 'red' }
+      if(rand == 2){ bgcolor = 'blue' }
+      if(rand == 3){ bgcolor = 'green' }
+      document.getElementById( target ).style.backgroundColor = bgcolor
+    },
+
   },
 })
-
 
