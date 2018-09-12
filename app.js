8e815273eeb7674813419a073ea8df6451317577
dev-yamaken-0528 <dev.yamaken.0528@gmail.com>
2018-09-12 17:23:45 +0900

    背景色をランダムに設定

--- app.js
+++ app.js
@@ -7,6 +7,42 @@ var app = new Vue({
   },
   mounted: function(){
     this.level = document.getElementById('level').value
+
+    var min = 1
+    var max = 3
+    var rand = 0
+    var row_max = 0
+    var col_max = 0
+
+    if(this.level == 'easy')  {
+      row_max = 5
+      col_max = 3
+    }
+    if(this.level == 'normal'){
+      row_max = 7
+      col_max = 5
+    }
+    if(this.level == 'hard')  {
+      row_max = 9
+      col_max = 7
+    }
+
+    var colcount = 0
+
+    for(var row_idx=0; row_idx<row_max; row_idx++){
+      var t_col_max = ((row_idx%2)==0) ? (col_max-1) : col_max
+      for(var col_idx=0; col_idx<t_col_max; col_idx++){
+        rand = Math.floor( Math.random() * (max + 1 - min) ) + min
+        var bgcolor = ''
+        if(rand == 1){ bgcolor = 'red' }
+        if(rand == 2){ bgcolor = 'blue' }
+        if(rand == 3){ bgcolor = 'green' }
+        document.getElementById( row_idx + '_' + colcount ).style.backgroundColor = bgcolor
+        //console.log("row["+row_idx+"] col["+colcount+"] rand["+rand+"] bgcolor["+bgcolor+"]")
+        colcount++
+      } 
+    }
+
   },
   methods: {
     clickhex: function(event){
@@ -33,24 +69,24 @@ var app = new Vue({
       var hex_id_d_l = (row+1) + delimiter + (col+(calc_max_num-1))
       var hex_id_d_r = (row+1) + delimiter + (col+calc_max_num)
 
-      if(document.getElementById(hex_id_l) != null){
-        document.getElementById(hex_id_l).style.backgroundColor = "red"
-      }
-      if(document.getElementById(hex_id_r) != null){
-        document.getElementById(hex_id_r).style.backgroundColor = "red"
-      }
-      if(document.getElementById(hex_id_u_l) != null){
-        document.getElementById(hex_id_u_l).style.backgroundColor = "blue"
-      }
-      if(document.getElementById(hex_id_u_r) != null){
-        document.getElementById(hex_id_u_r).style.backgroundColor = "blue"
-      }
-      if(document.getElementById(hex_id_d_l) != null){
-        document.getElementById(hex_id_d_l).style.backgroundColor = "green"
-      }
-      if(document.getElementById(hex_id_d_r) != null){
-        document.getElementById(hex_id_d_r).style.backgroundColor = "green"
-      }
+      //if(document.getElementById(hex_id_l) != null){
+      //  document.getElementById(hex_id_l).style.backgroundColor = "red"
+      //}
+      //if(document.getElementById(hex_id_r) != null){
+      //  document.getElementById(hex_id_r).style.backgroundColor = "red"
+      //}
+      //if(document.getElementById(hex_id_u_l) != null){
+      //  document.getElementById(hex_id_u_l).style.backgroundColor = "blue"
+      //}
+      //if(document.getElementById(hex_id_u_r) != null){
+      //  document.getElementById(hex_id_u_r).style.backgroundColor = "blue"
+      //}
+      //if(document.getElementById(hex_id_d_l) != null){
+      //  document.getElementById(hex_id_d_l).style.backgroundColor = "green"
+      //}
+      //if(document.getElementById(hex_id_d_r) != null){
+      //  document.getElementById(hex_id_d_r).style.backgroundColor = "green"
+      //}
     },
   },
 })
